e50035f764d60ac01c48cc02929f63416d2b2ce4
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-04-16 20:47:43 +0600

    Added Posts.js and Form.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import "./App.css";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
@@ -9,6 +12,18 @@ function App() {
           <Typography variant="h2" align="center"></Typography>
           <img src="/images/memories.jpg" alt="memories" height="600" />
         </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch">
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
       </Container>
     </div>
   );
