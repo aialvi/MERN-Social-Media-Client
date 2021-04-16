a41049c51fe13cf2d13f8fb4ec8b62fbc38a5868
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-04-16 21:25:46 +0600

    Added styles

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import "./App.css";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
+import useStyles from "./styles";
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <Container maxWidth="lg">
-        <AppBar position="static" color="inherit">
-          <Typography variant="h2" align="center"></Typography>
-          <img src="/images/memories.jpg" alt="memories" height="600" />
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">
+            MemoryTrek
+          </Typography>
+          <img
+            className={classes.image}
+            src="/images/memories.jpg"
+            alt="memories"
+            height="60"
+          />
         </AppBar>
         <Grow in>
           <Container>
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+import useStyles from "./styles";
 const Post = () => {
+  const classes = useStyles();
   return <h2>POST</h2>;
 };
 
