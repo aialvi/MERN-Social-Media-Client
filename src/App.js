1a0c9a5c9916e40b7ad4e8c6bbf3f6953489028f
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-04-18 19:41:43 +0600

    Added Axios and Redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts";
 import "./App.css";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
@@ -7,6 +9,11 @@ import useStyles from "./styles";
 
 function App() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <div className="App">
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 import useStyles from "./styles";
 const Posts = () => {
+  const posts = useSelector();
   const classes = useStyles();
   return (
     <>
